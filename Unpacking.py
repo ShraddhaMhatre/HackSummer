82e63d857cc82d279af88983d06c49f6cad3ef0f
Shraddha Mhatre <shraddha92mhatre@gmail.com>
2021-02-10 23:13:49 -0500

    Learn Python The Hard Way: Unpacking

---
+++ Unpacking.py
@@ -0,0 +1,10 @@
+from sys import argv
+#Unpacking
+#python main.py Harry James Potter
+script, first, middle, last = argv
+
+print("This will be your script: ", script)
+print("Enter your first name: ", first)
+print("Enter your middle name: ", middle)
+print("Enter your last name: ", last)
+
